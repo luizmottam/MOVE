3a3a068af8e994e6da325d75a462bcecbb4ead96
Luiz Motta <luizdamm@gmail.com>
2025-07-22 10:50:58 -0300

    Adição de imagens dos produtos

---
+++ move-web/src/components/ProductsCard/index.jsx
@@ -0,0 +1,9 @@
+import './ProductsCard.css';
+
+export default function ProductsCard() {
+    return (
+        <div className="products-card">
+            <img className='products-card-img' src="/img/utv.jpg" alt="produtos" />
+        </div>
+    )
+}
--- move-web/src/pages/Home/index.jsx
+++ move-web/src/pages/Home/index.jsx
@@ -1,14 +1,21 @@
 import "./Home.css";
 import Header from "../../components/Header";
 import Booking from "../../components/Booking";
+import ProductsCard from "../../components/ProductsCard";
 
 const Home = () => {
-
-
   return (
     <div className="home-container">
-      <Header/>
-      <Booking/>
+      <Header />
+      <div className="home-content">
+        <div className="booking-container">
+          <Booking />
+          <ProductsCard />
+        </div>
+        <div className="category-container">
+          
+        </div>
+      </div>
     </div>
   );
 };

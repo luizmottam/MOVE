f11340d08781268c2052bf1cdd363ff7c6955549
Luiz Motta <luizdamm@gmail.com>
2025-08-28 23:40:11 -0300

    Alinhamento do container e layout de produtos

--- frontend/src/pages/Veiculos/index.jsx
+++ frontend/src/pages/Veiculos/index.jsx
@@ -6,18 +6,18 @@ export default function Veiculos({ tipo }) {
 
   return (
     <>
-      <section className="px-[var(--dp)] py-5">
+      <section className="px-[var(--dp)] py-5 w-[100%]">
         <p className="uppercase">HOME / {tipo}</p> 
-        <h1 className="text-5xl font-bold uppercase text-center">{tipo}</h1>
-        <div>
-            <div>Slidebar</div>
-            <div>
-                <div>
+        <h1 className="text-5xl font-bold uppercase text-center mb-5">{tipo}</h1>
+        <div className="flex">
+            <div className="bg-white w-[30%] h-[50rem] border rounded-xl p-5">Slidebar</div>
+            <div className="flex flex-col w-[100%] p-5">
+                <div className="flex justify-between">
                     <p>100 anúncos encontrados</p>
                     <p>Ordenar por: Mas relevante</p>
                 </div>
-                <div className="">
-
+                <div className="grid grid-cols-4">
+                  
                 </div>
             </div>
         </div>
